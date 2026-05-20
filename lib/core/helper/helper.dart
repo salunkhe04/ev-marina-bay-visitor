9be7a7c65fb39407bb64650bc658ea44e799aa9d
unknown <salunkheshreya19@gmail.com>
2026-05-20 16:23:01 +0530

    adeed

---
+++ lib/core/helper/helper.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:marina_bay_cell_building_visitors/main.dart';
+
+class Helper {
+  static void showCustomSnackBar(
+    String message, [
+    Color bgColor = Colors.red,
+    Color textColor = Colors.white,
+  ]) {
+    final snackBar = SnackBar(
+      content: Text(message, style: TextStyle(color: textColor)),
+      behavior: SnackBarBehavior.floating,
+      margin: const EdgeInsets.all(16),
+      backgroundColor: bgColor,
+    );
+
+    if (scaffoldMessengerKey.currentState != null) {
+      // Show SnackBar if ScaffoldMessenger is available
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        scaffoldMessengerKey.currentState?.clearSnackBars();
+        scaffoldMessengerKey.currentState?.showSnackBar(snackBar);
+      });
+    } else {
+      // Show fallback dialog if no Scaffold is available
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (navigatorKey.currentContext != null) {
+          showDialog(
+            context: navigatorKey.currentContext!,
+            builder: (context) {
+              return AlertDialog(
+                backgroundColor: bgColor,
+                content: Text(message, style: TextStyle(color: textColor)),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: Text('OK', style: TextStyle(color: textColor)),
+                  ),
+                ],
+              );
+            },
+          );
+        } else {
+          // print("no context avail");
+          // Log or handle the case where neither context is available
+        }
+      });
+    }
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
 import file_picker
+import package_info_plus
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
